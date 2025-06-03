bf0af663b4c2c16784141cd5633c659b48c43ac0
Dongyub <ehdduq625@gmail.com>
2025-06-03 11:39:44 +0000

    Update definitions for using DASK

--- sidm/tools/scaleout.py
+++ sidm/tools/scaleout.py
@@ -2,12 +2,33 @@
 
 from dask.distributed import Client, PipInstall
 
-
 def make_dask_client(address):
     """Create dask client that includes dependency installer"""
     dependencies = [
-        "git+https://github.com/btcardwell/SIDM.git",
+        "git+https://github.com/yeopjjang/SIDM.git",
     ]
     client = Client(address)
     client.register_plugin(PipInstall(packages=dependencies, pip_options=["--upgrade"]))
     return client
+
+# """Module to define classes and methods that are helpful for scaleout"""
+
+# from dask.distributed import Client, PipInstall
+# import os
+
+    
+# def set_env(dask_worker):
+#     os.environ["XCACHE_HOST"] = "xcache.cmsaf-dev.flatiron.hollandhpc.org"
+
+
+# def make_dask_client(address):
+#     """Create dask client that includes dependency installer"""
+#     dependencies = [
+#         # "git+https://github.com/btcardwell/SIDM.git",
+#         "git+https://github.com/yeopjjang/SIDM.git",
+#     ]
+#     client = Client(address)
+#     client.register_plugin(PipInstall(packages=dependencies, pip_options=["--upgrade"]))
+#     client.run(set_env)
+    
+#     return client
